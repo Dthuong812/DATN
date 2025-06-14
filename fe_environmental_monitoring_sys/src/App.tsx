dbfa37d618daa4adc71d14c62653b3a23b79bf04
Thuong <tranthidathuong812@gmail.com>
2025-06-14 22:33:23 +0700

    setup

---
+++ fe_environmental_monitoring_sys/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <div className="p-6 bg-gray-100 min-h-screen">
+      <Button variant="default" className="text-black">Nút ShadCN</Button>
+    </div>
+  )
+}
+
+export default App
